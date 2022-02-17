c427e5ff6fac4c333bb8222f8c908950079aec66
kssai <kssai@DESKTOP-KTDB25U>
2022-02-17 19:21:18 +0530

    Repeating substring

---
+++ logical_programming_zoho/src/logical_programming/ReapeatingSubString.java
@@ -0,0 +1,61 @@
+package logical_programming;
+
+ 
+import java.util.*;
+
+class ReapeatingSubString {
+	 public static void main(String[] args) {
+		 String str = "However Everyday is a day";
+//		 String str = "i love my country india";
+//		 String str = "china china";
+//		 String str = "tahil istahiss";
+//		 String str = "hello google whats ur statsa googleeee";
+//		 String str = "my thailand name is thiliname land";
+//		 String str = "i am from inindia";
+
+		 str = str.toLowerCase();
+		 solve(str);
+   }
+	
+    public static void solve (String str){
+    	int length = str.length();
+       if(length <= 3){
+           return ;
+       }
+       HashSet<String> hset = new HashSet<>();
+       int ptr  = 0;//ptr
+       while(ptr<length-2){
+           if(str.charAt(ptr)!= ' ' && str.charAt(ptr+1) != ' '){ //to eliminate space
+               int x = ptr+2;
+               //inner loop for checking
+               while(x<length){
+                  int wordLen =2;
+                  String tmp = "";
+                  while(x+wordLen-1 < str.length()){
+                	  String str1 = str.substring(ptr,ptr+wordLen);
+                	  String str2 = str.substring(x,x+wordLen);
+                      if( str1.equals(str2)) {
+                    	  tmp = str.substring(ptr,ptr+wordLen);
+                    	  wordLen++;
+                      }
+                      else
+                    	  break;      
+                  }
+                  if(tmp.length()>1){
+                	  ptr = ptr+wordLen-1;
+                      hset.add(tmp);
+                  }
+                   x++;
+               }
+           }
+           ptr++;
+       }
+       if(!hset.isEmpty()) {
+    	   for(String s: hset)
+           	System.out.println(s);   
+       }
+       else
+    	   System.out.println("none");
+    }
+   
+}
