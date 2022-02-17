7129649b10f07939e1d6bacc47e617f8d86db4ed
kssai <kssai@DESKTOP-KTDB25U>
2022-02-17 13:03:46 +0530

    optimized

--- logical_programming_zoho/src/logical_programming/FindNextGreaterElement.java
+++ logical_programming_zoho/src/logical_programming/FindNextGreaterElement.java
@@ -12,23 +12,19 @@ import java.util.*;
 public class FindNextGreaterElement {
 
 	public static void main(String[] args) {
-		int a[] = {2,5,7};
+//		int a[] = {2,5,7};
 //		int a[] = {2, 4, 8, 90, 77, 54};
-//		int a[] = {-1,2,6,1,10,23,4,5};//1 4 10 4 23 -1 5 -1 
+		int a[] = {-1,2,6,1,10,23,4,5};//1 4 10 4 23 -1 5 -1 
 		int n = a.length;
 		int result[] = new int[n];
-		result[n-1] = -1;
 		TreeSet<Integer> set = new TreeSet<>();
 		
 		for(int i= n-1; i>=0; i--) {
-			
 			Integer val = set.higher(a[i]);
-			if(!set.isEmpty() && val != null) {
+			if(!set.isEmpty() && val != null)
 				result[i] = val;
-			}
 			else
 				result[i] = -1;
-			
 			set.add(a[i]);
 		}
 		for(int i=0; i<n; i++)
