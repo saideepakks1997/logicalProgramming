198da9c658d56e57d10b86251b4a49d6d1ec053a
kssai <kssai@DESKTOP-KTDB25U>
2022-02-22 09:57:51 +0530

    Brute force program for repeating subsequence

---
+++ logical_programming_zoho/src/logical_programming/RepeatingSubString2.java
@@ -0,0 +1,32 @@
+package logical_programming;
+
+public class RepeatingSubString2 {
+	public static void main(String args[]) {
+		int a[] = {1,0,0,1,0,1,1,1};
+//		int a[] = {1,0};
+//		int a[] = {1,1,1,0,0,0,1};
+//		int a[] = {1,1,1,1,1,1,0};
+//		int a[] = {0,1,1,1,1,1,1,0};
+//		int a[] = {0,0,0,0,0,0,0};
+		int start = 0,end = 0,maxLength = 0;
+		int zeros = 0,ones = 0;
+		for(int i=0; i<a.length; i++) {
+			zeros = 0;
+			ones=0;
+			int dummy = (a[i] == 0)?zeros++:ones++;
+			for(int j=i+1; j<a.length;j++) {
+				dummy = (a[j] == 0)?zeros++:ones++;
+				int len = j-i+1;
+				if(ones == zeros && len > maxLength) {
+					start = i;
+					end = j;
+					maxLength = len;
+				}
+			}
+		}
+		if(maxLength > 1)
+			System.out.println(start+" to "+end);
+		else
+			System.out.println("No sub Arrays found");
+	}
+}
