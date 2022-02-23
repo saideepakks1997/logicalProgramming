a4361fe64fd3915cbd5fdaa5f9df20da1618325b
kssai <kssai@DESKTOP-KTDB25U>
2022-02-23 13:47:47 +0530

    admin

--- solid_interface_segregation_principle/src/user/IAdmin.java
+++
@@ -1,9 +0,0 @@
-package user;
-
-import theatre.Theatre;
-
-public interface IAdmin extends IUser{
-	public void bulkBooking(Theatre theatre);
-	public void updateTicketPrice(Theatre theatre);
-	public void updateSnacksPrice(Theatre theatre);
-}
